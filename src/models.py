a7052e1b15965744eceab86236ce89bf7520d5a3
Jose Reimondez <josereimondez29@gmail.com>
2024-03-15 19:23:06 +0000

    avanzando

--- src/models.py
+++ src/models.py
@@ -3,13 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+    favorites = db.relationship("Favorite", back_populates="user")
+
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "User {} de nombre {}".format(self.id, self.name)
 
     def serialize(self):
         return {
