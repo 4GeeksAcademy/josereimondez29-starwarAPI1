160bdc415fb0c43781be0d080dc3a4cf5291fb0d
Jose Reimondez <josereimondez29@gmail.com>
2024-03-20 18:27:35 +0000

    finitto

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Planet, Character, Vehicle_Starship
+from models import db, User, Planet, Character, Vehicle_Starship, FavoritePlanet,FavoriteCharacter,FavoriteVehicleStarship
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,9 +12,11 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(FavoritePlanet, db.session))
     admin.add_view(ModelView(Character, db.session))
-    
+    admin.add_view(ModelView(FavoriteCharacter, db.session))
     admin.add_view(ModelView(Vehicle_Starship, db.session))
+    admin.add_view(ModelView(FavoriteVehicleStarship, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -38,15 +38,27 @@ class Planet(db.Model):
         return {
             "id": self.id,
             "name": self.name,
+            "climate": self.climate,
+            "population": self.population
         }
     
 class FavoritePlanet(db.Model):
     __tablename__ = 'favorite_planets'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id_relationship = db.relationship(User)
     planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
     planet_relationship = db.relationship(Planet) 
     
+    def __repr__(self):
+        return f"Usuario {self.id} le gusta planeta {self.planet_id}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
+        }
 
 class Character(db.Model):
     __tablename__ = 'character'
@@ -63,13 +75,28 @@ class Character(db.Model):
         return {
             "id": self.id,
             "name": self.name,
+            "species": self.species,
+            "gender": self.gender
         }
+    
 class FavoriteCharacter(db.Model):
     __tablename__ = 'favorite_characters'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id_relationship = db.relationship(User)
     character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
     character_relationship = db.relationship(Character) 
+
+    def __repr__(self):
+        return f"Usuario {self.id} le gusta character {self.character_id}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id
+        }
+
 
 class Vehicle_Starship(db.Model):
     __tablename__ = 'vehicle_starship'
@@ -89,11 +116,32 @@ class FavoriteVehicleStarship(db.Model):
     __tablename__ = 'favorite_vehicles_starships'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id_relationship = db.relationship(User)
     vehicle_starship_id = db.Column(db.Integer, db.ForeignKey('vehicle_starship.id'))
     vehicle_starship_relationship = db.relationship(Vehicle_Starship) 
 
+    def __repr__(self):
+        return f"Usuario {self.id} le gusta vehiculo {self.vehicle_starship_id}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "vehicle_starship_id": self.vehicle_starship_id
+        }
 
-
+'''class Favorite(db.Model):
+    __tablename__ = 'favorite_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_relationship = db.relationship(User)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+    planet_relationship = db.relationship(Planets)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    character_relationship = db.relationship(Character)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle_starship.id'), nullable=False)
+    character_relationship = db.relationship(Vehicle_Starship)
+    '''
 
 
    
